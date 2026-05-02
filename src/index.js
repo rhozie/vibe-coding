29189115990d3e1b33ad2301481a133d649192a2
rhozie <zhiie909@gmail.com>
2026-05-02 14:55:41 +0700

    feat: implement user registration API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import { Elysia } from 'elysia';
 import { userRoutes } from './routes/user.js';
+import { usersRoutes } from './routes/users-routes.js';
 
 const PORT = process.env.PORT || 3000;
 
 const app = new Elysia()
   .get('/', () => ({ message: 'API is running 🦊' }))
   .use(userRoutes)
+  .use(usersRoutes)
   .listen(PORT);
 
 console.log(`🦊 Elysia is running at http://localhost:${PORT}`);
